3a721efb2fdd0e50a559f4b5e12e0384ec72ea39
Lars <leonteddy@hotmail.de>
2026-01-07 22:57:17 +0100

    moved scripts

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
 document.getElementById("year").textContent = new Date().getFullYear();
+
+document.getElementById("company").addEventListener("click", () => {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+
+    history.replaceState(null, document.title, window.location.pathname + window.location.search);
+});
+
+document.getElementById("contact-form").addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const name = document.getElementById("name").value;
+    const message = document.getElementById("message").value;
+
+    const numbers = [105, 110, 102, 111, 64, 105, 116, 45, 115, 101, 114, 118, 105, 99, 101, 45, 115, 116, 101, 105, 110, 107, 97, 109, 112, 46, 100, 101];
+    const subject = encodeURIComponent("Kontaktanfrage " + name);
+    const body = encodeURIComponent(
+        "Name:\n" + name + "\n\n" +
+        "Nachricht:\n" + message
+    );
+
+    window.location.href = `mailto:Jan%20Steinkamp<${String.fromCharCode(...numbers)}>?subject=${subject}&body=${body}`;
+})
